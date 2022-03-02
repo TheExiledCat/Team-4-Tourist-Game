264acf556018de980f9189553637e6aa1e1cf520
Stella Rübsaam <stella.rubsaam@outlook.com>
2022-03-02 15:10:04 +0100

    feat(UI): add simple structure, canvasgroup and reference to this canvasgroup

---
+++ DutCheese/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour
+{
+    [SerializeField]
+    CanvasGroup m_DialogueBox;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
